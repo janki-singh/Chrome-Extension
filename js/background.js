518ef05c603b07dccc7081686342f65d16a4cfc8
Janki Singh <121485112+janki-singh@users.noreply.github.com>
2024-03-18 00:04:53 +0530

    Add files via upload

---
+++ js/background.js
@@ -0,0 +1,17 @@
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    console.log(alarm.name);
+    chrome.tabs.create({
+        active: true,
+        url: alarm.name
+    }, null);
+});
+
+chrome.notifications.onClicked.addListener(() => {
+    console.log("Happen");
+});
+
+chrome.commands.onCommand.addListener((command) => {
+    console.log(`Command "${command}" called`);
+});
+
+
